77264c1c7df50352187603ef0119603bdbfbf7d1
cTenKaTJK <99907848+cTenKaTJK@users.noreply.github.com>
2022-05-09 21:50:04 +0200

    123

---
+++ funcs.py
@@ -0,0 +1,68 @@
+import datetime
+import json
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    olymp = StringField('Название олимпиады', validators=[DataRequired()])
+    login = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Сохранить данные')
+
+
+def unpack(olymp_list):
+    olymps = []
+    n = 1
+    for olymp in olymp_list:
+        olymp_name = olymp
+        quals, finals = [], []
+        for qual in olymp_list[olymp]["Отборочные"]:
+            quals.append({
+                "id": n,
+                "name": olymp_name,
+                "stage": "Отборочный этап",
+                "date": qual[0],
+                "desc": qual[1],
+                "place": "Онлайн",
+                "reg": False,
+                "link": qual[2]})
+            n += 1
+        for final in olymp_list[olymp]["Заключительные"]:
+            finals.append({
+                "id": n,
+                "name": olymp_name,
+                "stage": "Заключительный этап",
+                "date": final[0],
+                "desc": final[1],
+                "place": final[2],
+                "reg": False,
+                "link": final[3]})
+            n += 1
+        olymps.extend(quals)
+        olymps.extend(finals)
+
+    for a in range(len(olymps) - 1):
+        for el in range(len(olymps) - a - 1):
+            d1, m1, y1 = map(int, olymps[el]["date"].split('.'))
+            d2, m2, y2 = map(int, olymps[el + 1]["date"].split('.'))
+            date_1 = datetime.date(year=y1, month=m1, day=d1)
+            date_2 = datetime.date(year=y2, month=m2, day=d2)
+            if date_1 > date_2:
+                olymps[el], olymps[el + 1] = olymps[el + 1], olymps[el]
+    return olymps
+
+
+def pu(olymps):
+    olymps_passed = []
+    olymps_upcoming = []
+    for b in olymps:
+        d, m, y = map(int, b["date"].split('.'))
+        date = datetime.date(year=y, month=m, day=d)
+        if date < datetime.date.today():
+            olymps_passed.append(b)
+        else:
+            olymps_upcoming.append(b)
+    return olymps_passed, olymps_upcoming
+
--- main.py
+++ main.py
@@ -1,64 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 import json
 import datetime
+from funcs import unpack, pu, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'bebra'
+
+logins = []
+user_login = False
 
 
-def unpack(olymp_list):
-    olymps = []
-    for olymp in olymp_list:
-        olymp_name = olymp
-        quals, finals = [], []
-        for qual in olymp_list[olymp]["Отборочные"]:
-            quals.append({
-                "name": olymp_name,
-                "stage": "Отборочный этап",
-                "date": qual[0],
-                "desc": qual[1],
-                "place": "Онлайн",
-                "link": qual[2]})
-        for final in olymp_list[olymp]["Заключительные"]:
-            finals.append({
-                "name": olymp_name,
-                "stage": "Заключительный этап",
-                "date": final[0],
-                "desc": final[1],
-                "place": final[2],
-                "link": final[3]})
-        olymps.extend(quals)
-        olymps.extend(finals)
-
-    for a in range(len(olymps) - 1):
-        for el in range(len(olymps) - a - 1):
-            d1, m1, y1 = map(int, olymps[el]["date"].split('.'))
-            d2, m2, y2 = map(int, olymps[el + 1]["date"].split('.'))
-            date_1 = datetime.date(year=y1, month=m1, day=d1)
-            date_2 = datetime.date(year=y2, month=m2, day=d2)
-            if date_1 > date_2:
-                olymps[el], olymps[el + 1] = olymps[el + 1], olymps[el]
-    olymps_passed = []
-    olymps_upcoming = []
-    for b in olymps:
-        d, m, y = map(int, b["date"].split('.'))
-        date = datetime.date(year=y, month=m, day=d)
-        if date < datetime.date.today():
-            olymps_passed.append(b)
-        else:
-            olymps_upcoming.append(b)
-    return olymps_passed, olymps_upcoming
-
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/',  methods=['POST', 'GET'])
+@app.route('/home',  methods=['POST', 'GET'])
 def based():
-    with open("example.json", "rt", encoding="utf8") as Jason_Brody:
-        f = Jason_Brody.read()
-        olymp_list = json.loads(f)
+    if user_login:
+        with open("example.json", "rt", encoding="utf8") as Jason_Brody:
+            f = Jason_Brody.read()
+            olymp_list = json.loads(f)
+        olymps = unpack(olymp_list)
+        olymps_passed, olymps_upcoming = pu(olymps)
+    else:
+        olymps_passed = olymps_upcoming = []
     months = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"]
     d = datetime.date.today()
     date = str(d.day) + " " + months[d.month - 1] + " " + str(d.year)
-    olymps_passed, olymps_upcoming = unpack(olymp_list)
     return render_template('base.html', title="сайт", olymps_passed=olymps_passed, olymps_upcoming=olymps_upcoming, current_date=date)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    dt = LoginForm().data
+    if dt["submit"]:
+        global user_login
+        user_login = True
+        logins.append(dt)
+        with open("olymp_logins.json", "w", encoding="utf8") as file:
+            json.dump(logins, file, ensure_ascii=False, indent=2)
+    return render_template('login_field.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
